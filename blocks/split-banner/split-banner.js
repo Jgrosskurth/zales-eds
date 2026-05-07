3c8f0d8778a6f9d01c202916e463356ed5f00df5
Jgrosskurth <159270531+Jgrosskurth@users.noreply.github.com>
2026-05-07 02:22:39 +0000

    rebuild files

--- blocks/split-banner/split-banner.js
+++ blocks/split-banner/split-banner.js
@@ -74,7 +74,7 @@ export default function decorate(block) {
         io.unobserve(block);
       }
     },
-    { threshold: 0.12 },
+    { threshold: 0.05, rootMargin: '300px 0px 300px 0px' },
   );
   io.observe(block);
 }
